a44ef2b3a54918f3dd3c268cad65645fe9a540a6
Jimmy Fonteneau <jimmyfo1307@yahoo.fr>
2019-11-08 13:10:23 +0100

    Applications : Add toolbar

--- intranet_phonecenter/settings.py
+++ intranet_phonecenter/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'credits',
     'customer',
     'supports',
-    'users',
+    'users',    
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'intranet_phonecenter.urls'
@@ -123,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+INTERNAL_IPS = [    
+    '127.0.0.1',   
+]
--- intranet_phonecenter/urls.py
+++ intranet_phonecenter/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
 import users
 import customer
 import credits
@@ -29,3 +31,13 @@ urlpatterns = [
     path(r'calls/', include('calls.urls', namespace='calls')),
     path(r'supports/', include('supports.urls', namespace='supports')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+
+        # For django versions before 2.0:
+        # url(r'^__debug__/', include(debug_toolbar.urls)),
+
+    ] + urlpatterns
