ffd57fa19c3c5e8af2e6d0ba765df2be7a4d11d2
Jimmy Fonteneau <jimmyfo1307@yahoo.fr>
2019-11-08 13:39:46 +0100

    Template : Settings paths

--- intranet_phonecenter/settings.py
+++ intranet_phonecenter/settings.py
@@ -61,7 +61,9 @@ ROOT_URLCONF = 'intranet_phonecenter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'intranet_phonecenter', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # View.
 def hello(request):
     print("Bonjour")
-    return HttpResponse("Hello word!")
+    # return HttpResponse("Hello word!")
+    return JsonResponse({ 'message': 'hello' })
