0f7ab5cf78856286ad8bdaa392393288a8b179ef
Jimmy Fonteneau <jimmyfo1307@yahoo.fr>
2019-11-25 09:37:05 +0100

    Users : create UserProfile model

--- users/views.py
+++ users/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 
 # View.
-def hello(request):
+def hello_old(request):
     print("Bonjour")
     # return HttpResponse("Hello word!")
-    return JsonResponse({ 'message': 'hello' })
+    return JsonResponse({ 'message': 'hello worold' })
+
+def hello(request):
+    return render (
+        request,
+        'users/hello.html',
+        {
+            'message': "Hello world"
+        }
+    )
